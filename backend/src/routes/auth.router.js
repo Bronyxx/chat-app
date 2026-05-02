cb0cbe079ab63285be672f1a6b318c647f721c0f
sreehari <sreeharii859@gmail.com>
2026-05-02 17:53:25 +0530

    rate limiting implemented

--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {signup,login,logout,updateProfilePic} from "../controllers/auth.controller.js"
 import {protectRoute} from "../middlewares/auth.middlewares.js"
+import {arcjetProtection} from "../middlewares/arcjet.middlewares.js"
 const router= express.Router();
 
+router.use(arcjetProtection);
 router.post("/signup",signup);
 
 router.post("/login",login);
